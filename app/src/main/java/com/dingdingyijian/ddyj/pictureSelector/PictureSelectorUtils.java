f1e7780e04ced31ecdf362b9247ed89ed6488299
Tyh-RedmiBook <15016709483@163.com>
2022-03-29 22:24:13 +0800

    优化加载图片

--- app/src/main/java/com/dingdingyijian/ddyj/pictureSelector/PictureSelectorUtils.java
+++ app/src/main/java/com/dingdingyijian/ddyj/pictureSelector/PictureSelectorUtils.java
@@ -1,6 +1,5 @@
 package com.dingdingyijian.ddyj.pictureSelector;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
